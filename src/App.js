8d4c28511958e67789e1ba5b0a2c4ae5111080d7
Ricardo Maya <rickmaya@gmail.com>
2022-04-22 20:04:52 -0500

    create and add about me page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Route, Routes} from "react-router-dom";
 import Landing from "./components/Landing/landing";
+import AboutMe from "./components/AboutMe/aboutMe";
 
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<Landing />} />
+      <Route path="/about" element={<AboutMe />} />
     </Routes>
   );
 }
