969e59a56fa670050d998ab0d0ac617f4a8c6849
Ricardo Maya <rickmaya@gmail.com>
2023-01-19 23:32:44 -0600

    remove reportWebVitals

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
